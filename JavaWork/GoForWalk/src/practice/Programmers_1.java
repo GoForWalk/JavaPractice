7fc48fb020c7e20748ec96e0b67eba6c64f9152a
kit-lec <atcgsp912@gmail.com>
2020-04-27 17:27:49 +0900

    Java Practice

--- JavaWork/GoForWalk/src/practice/Programmers_1.java
+++ JavaWork/GoForWalk/src/practice/Programmers_1.java
@@ -18,53 +18,90 @@ public class Programmers_1 {
         System.out.println(s.solution(board,moves));
 
 
+
     }
 }
 
+//class Solution {
+//    public int solution(int[][] board, int[] moves) {
+//
+//        int answer = 0;
+//        boolean hasSame = true;
+//        ArrayList<Integer> box = new ArrayList<Integer>();
+//
+//        for(int k = 0; k < moves.length; k++) {
+//
+//            int j = moves[k] - 1 ;
+//
+//            for (int i = 0; i < board.length; i++) {
+//
+//                if (board[i][j] != 0) {
+//                    box.add(board[i][j]);
+//                    board[i][j] = 0;
+//
+//                    break;
+//                }
+//            }
+//
+//
+//
+//
+//
+//        }
+//
+//        while (hasSame) {
+//
+//            for (int i = 0; i < box.size(); i++) {
+//
+//                if (box.get(i) == box.get(i - 1)) {
+//                    answer += 2;
+//                    box.remove(i - 1);
+//                    box.remove(i);
+//                    hasSame = true;
+//                    break;
+//                }
+//                hasSame = false;
+//            }
+//
+//        }
+//        return answer;
+//
+//    } // end solution()
+
+//}// end Solution
+
 class Solution {
     public int solution(int[][] board, int[] moves) {
-
         int answer = 0;
-        boolean hasSame = true;
-        ArrayList<Integer> box = new ArrayList<Integer>();
+        int[] basket = new int[moves.length];
+        int c = 0;
 
-        for(int k = 0; k < moves.length; k++) {
+        for (int a = 0; a < moves.length; a++){ // 크레인이 움직인 횟수만큼 시행
 
-            int j = moves[k] - 1 ;
+            for (int i = 0; i < board[moves[a] - 1].length; i++){ // 크레인이 집을 인형이 있는 라인의 맨 윗칸부터 인형찾기
 
-            for (int i = 0; i < board.length; i++) {
+                if (board[i][moves[a] - 1] != 0){
 
-                if (board[i][j] != 0) {
-                    box.add(board[i][j]);
-                    board[i][j] = 0;
+                    for (int b = 0; b < basket.length; b++){
 
-                    break;
+                        if (basket[b] == 0){
+                            basket[b] = board[i][moves[a] - 1];
+                            board[i][moves[a] - 1] = 0;
+                            c = b;
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }//end for
+
+                    if (c > 0 && basket[c] == basket[c - 1]){
+                        answer += 2;
+                        basket[c] = 0;
+                        basket[c - 1] = 0;
+                    }
                 }
             }
-
-
-
-
-
-        }
-
-        while (hasSame) {
-
-            for (int i = 0; i < box.size(); i++) {
-
-                if (box.get(i) == box.get(i - 1)) {
-                    answer += 2;
-                    box.remove(i - 1);
-                    box.remove(i);
-                    hasSame = true;
-                    break;
-                }
-                hasSame = false;
-            }
-
         }
         return answer;
-
-    } // end solution()
-
-}// end Solution
+    }
+}
--- JavaWork/GoForWalk/src/practice/Programmers_2.java
+++ JavaWork/GoForWalk/src/practice/Programmers_2.java
@@ -10,9 +10,40 @@ public class Programmers_2 {
 
     class Solution {
         public int[] solution(int n) {
-            int arrLength_1 = 0;
 
-            arrLength_1 = 2 * n - 1;
+            int arrLength = (int)Math.pow(2,n) - 1;
+
+//            int middle = (int)Math.pow(2, n)/2 ;
+
+            int[] arrInt = new int[arrLength];
+
+
+//            for(int i = middle - 1; i > 0 ; i-- ){
+//
+//            }
+
+            for(int i = 0; i < n; i++){
+
+                int middle = (int)Math.pow(2, i)/2 ;
+                int [] arrInt_priv = new int[(int)Math.pow(2,i)];
+
+
+                arrInt_priv[middle] = 0;
+
+                for(int j = middle; j > 0; j--){
+
+
+
+                }
+
+            }
+
+
+
+
+
+
+
 
 
 
