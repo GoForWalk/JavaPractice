1d015c40efec41818b3027063a154bfdcdd0b88e
kit-lec <atcgsp912@gmail.com>
2020-04-30 23:19:19 +0900

    Html practice (20-04-30 practice)

--- JavaWork/GoForWalk/src/practice/Programmers_2.java
+++ JavaWork/GoForWalk/src/practice/Programmers_2.java
@@ -7,19 +7,43 @@ public class Programmers_2 {
 
         Solution s = new Solution();
 
-        s.solution(3);
 
+        s.solution(3);
 
 
     }
 
 
-
-    static class Solution {
+    static  class Solution {
         public int[] solution(int n) {
 
-            int[] answer = new int[(int) Math.pow(2, n) - 1];
 
+            int arrLengthAsr = (int) Math.pow(2, n) - 1;
+
+
+            int[] answer = new int[arrLengthAsr];
+
+            if (n > 1) {
+
+                int temp = n - 1 ;
+
+                for (int i = 0; i < temp; i++) {
+
+                    int middlePoint = (int) (Math.pow(2, i + 1)) / 2;
+                    int arrLength = (int) Math.pow(2, i);
+
+                    int[] arrSolution = new int[i + 1];
+                    for(int j = 0; j > arrLength; j++){
+                        arrSolution[j] = answer[j];
+                    }
+
+                    for (int j = 0; j > middlePoint; j++) {
+                        answer[j] = arrSolution[j];
+                        answer[arrLength - j] = (arrSolution[j] - 1) * -1;
+                    }
+
+                }
+            }
 
 
             return answer;
