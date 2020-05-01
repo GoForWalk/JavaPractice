29426239afd681c19eb5ef8df51c3079c18c8655
kit-lec <atcgsp912@gmail.com>
2020-05-01 19:06:44 +0900

    Html practice (20-05-01 programmers)

--- JavaWork/GoForWalk/src/practice/Programmers_2.java
+++ JavaWork/GoForWalk/src/practice/Programmers_2.java
@@ -17,35 +17,33 @@ public class Programmers_2 {
     static  class Solution {
         public int[] solution(int n) {
 
-
             int arrLengthAsr = (int) Math.pow(2, n) - 1;
-
 
             int[] answer = new int[arrLengthAsr];
 
             if (n > 1) {
 
-                int temp = n - 1 ;
+                int temp = n - 1;
 
-                for (int i = 0; i < temp; i++) {
+                for (int i = 1; i <= temp; i++) {
 
-                    int middlePoint = (int) (Math.pow(2, i + 1)) / 2;
-                    int arrLength = (int) Math.pow(2, i);
+//                    int middlePoint = (int) (Math.pow(2, i + 1)) / 2;
+                    int arrLength = (int) Math.pow(2, i) - 1;
+                    int nextArrLength = (int)Math.pow(2, i + 1) - 1;
 
-                    int[] arrSolution = new int[i + 1];
-                    for(int j = 0; j > arrLength; j++){
+                    int[] arrSolution = new int[arrLength];
+
+                    for(int j = 0; j < arrLength; j++){
                         arrSolution[j] = answer[j];
+                        answer[j] = arrSolution[j];
+                        answer[nextArrLength - 1 - j] = (arrSolution[j] - 1) * - 1;
                     }
 
-                    for (int j = 0; j > middlePoint; j++) {
-                        answer[j] = arrSolution[j];
-                        answer[arrLength - j] = (arrSolution[j] - 1) * -1;
-                    }
+//                    for (int j = 0; j > arrLength; j++) {
+//                    }
 
                 }
             }
-
-
             return answer;
         }
     }
